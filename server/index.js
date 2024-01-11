51dcb31d5a4576d9de545fad3ccb098c4b57acbc
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2024-01-11 18:03:26 -0300

    Added cors

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 app.use(cors());
 const apiRouter = require("./routes/api");
